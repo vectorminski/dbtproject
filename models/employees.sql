610743c1bb513d980f86c087d8a2f17afb651c02
vitordominski37@gmail.com <vitordominski37@gmail.com>
2022-12-27 14:47:47 +0000

    VERSAO 1.0

---
+++ models/employees.sql
@@ -0,0 +1,8 @@
+with calc_employees as (
+        select 
+            date_part(year, current_date) - date_part(year, birth_date) as age,
+            date_part(year, current_date) - date_part(year, hire_date) as lengthofservice,
+            first_name || ' ' || last_name name, *
+        from {{source('sources', 'employees')}}
+)
+select * from calc_employees
